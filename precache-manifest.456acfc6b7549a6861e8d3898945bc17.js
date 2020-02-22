ef91d71c54eecd7c6a7d22d9918edbb69413a7f3
Saphal <gsaphal99@gmail.com>
2020-02-22 17:02:48 +0545

    Updates

---
+++ precache-manifest.456acfc6b7549a6861e8d3898945bc17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f3343b992053885ef533f1b68c4f56",
+    "url": "/BuySell/index.html"
+  },
+  {
+    "revision": "0e32e818b043c4ca6c6b",
+    "url": "/BuySell/static/js/2.76419414.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/BuySell/static/js/2.76419414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4b168355f039d18ced",
+    "url": "/BuySell/static/js/main.0e26dbba.chunk.js"
+  },
+  {
+    "revision": "90c742da9411390b9cd6",
+    "url": "/BuySell/static/js/runtime-main.fd47ed3b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d68bfccef5cf67d2ee0540d239c0249f.js"
+  "/BuySell/precache-manifest.456acfc6b7549a6861e8d3898945bc17.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BuySell/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
