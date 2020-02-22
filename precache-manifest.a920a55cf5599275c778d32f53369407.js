d41c998c256b882cc714ff03560a6ae2c651c3cc
Saphal <gsaphal99@gmail.com>
2020-02-22 16:52:04 +0545

    Updates

---
+++ precache-manifest.a920a55cf5599275c778d32f53369407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f534617ae7eb5ef90745c1a422049dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e32e818b043c4ca6c6b",
+    "url": "./static/js/2.76419414.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.76419414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4b168355f039d18ced",
+    "url": "./static/js/main.0e26dbba.chunk.js"
+  },
+  {
+    "revision": "7b72b569fa27e0c78e6c",
+    "url": "./static/js/runtime-main.50fb443c.js"
+  }
+]);
