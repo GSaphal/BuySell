085d66e6fb3676c53a59539d07f88453ca9de1da
Saphal <gsaphal99@gmail.com>
2020-02-22 16:41:02 +0545

    Updates

---
+++ precache-manifest.a93b664ffefb60e5eb6b9743e4fec0d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90a5430dd14f0eb49e455ca97de703e",
+    "url": "/BuySell.git/index.html"
+  },
+  {
+    "revision": "0e32e818b043c4ca6c6b",
+    "url": "/BuySell.git/static/js/2.76419414.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/BuySell.git/static/js/2.76419414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4b168355f039d18ced",
+    "url": "/BuySell.git/static/js/main.0e26dbba.chunk.js"
+  },
+  {
+    "revision": "d249520209157e2f93cf",
+    "url": "/BuySell.git/static/js/runtime-main.07220e84.js"
+  }
+]);
